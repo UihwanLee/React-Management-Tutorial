5655979334876e2dd851128ed0563c6dd5902eef
UihwanLee <36596037+UihwanLee@users.noreply.github.com>
2020-06-04 22:57:37 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-Parser');
 const app = express();
@@ -6,33 +7,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+    
+connection.connect(function(error){
+    if(error) console.log(error);
+    else console.log("database connected");
+});
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': "https://placeimg.com/64/64/1",
-            'name': '홍길동',
-            'birthday': '990212',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': "https://placeimg.com/64/64/2",
-            'name': '이순신',
-            'birthday': '980251',
-            'gender': '남자',
-            'job': '회사원'
-        },
-        {
-            'id': 3,
-            'image': "https://placeimg.com/64/64/3",
-            'name': '김명동',
-            'birthday': '970524',
-            'gender': '남자',
-            'job': '취준생'
-        }
-    ]);
+    connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+        if(err) console.log(err)
+        else{
+            res.send(rows);
+        } 
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
